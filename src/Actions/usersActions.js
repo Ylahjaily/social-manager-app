0abf5ddef0c50af429d49f1d57a5c22a7d8458bb
Ylahjaily <y.lahjaily@ecole-ipssi.net>
2020-03-18 22:47:19 +0100

    JWT Login

--- src/Actions/usersActions.js
+++ src/Actions/usersActions.js
@@ -25,4 +25,42 @@ export function fetchAllUsers(){
     }
 }
 
-export default(fetchAllUsers);
+export function addUser(data){
+    return dispatch => {
+        return fetch(`${baseUrl}`+'/auth/register',{
+            method : 'POST',
+            body : JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept':'application/json',
+                'Access-Control-Allow-Origin' : '*'
+            },
+        })
+    }
+}
+
+export function login(data){
+    return dispatch => {
+        return fetch(`${baseUrl}`+'/api/auth/login',{
+            method : 'POST',
+            body : JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept':'application/json',
+                'Access-Control-Allow-Origin' : '*'
+            },
+        })
+        .then((response => {
+            response.json()
+            .then(result => {
+                console.log(result)
+                localStorage.setItem('login',JSON.stringify({
+                    login: true,
+                    token: result.token
+                }))
+            })
+        }))
+    }
+}
+
+export default(fetchAllUsers,addUser,login);
